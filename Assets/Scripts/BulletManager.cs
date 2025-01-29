29b94115868b1441cf42bd079595f39f81dfd96e
0wifi <104783815+0wifi@users.noreply.github.com>
2025-01-29 16:58:36 -0600

    the bullets are pooling

--- Assets/Scripts/BulletManager.cs
+++ Assets/Scripts/BulletManager.cs
@@ -4,16 +4,39 @@ using UnityEngine;
 
 public class BulletManager : MonoBehaviour
 {
-    private GameObject[] bulletPool = new GameObject[0];
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private GameObject bullet;
+
+    private Stack bulletPool = new Stack();
+
+    /// <summary>
+    ///     Should be called when an enemy shoots
+    /// 
+    ///     Gets bullet from pool; if pool is empty instantiates new bullet for use
+    /// </summary>
+    /// <returns> Bullet from pool </returns>
+    public GameObject GetBullet()
     {
-        
+        if (bulletPool.Count == 0)
+        {
+            GameObject newBullet = Instantiate(bullet);
+            return newBullet;
+        }
+        else
+        {
+            GameObject newBullet = (GameObject)bulletPool.Pop();
+            newBullet.transform.parent = null;
+            return newBullet;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    ///     Puts bullet into pool and sets as inactive
+    /// </summary>
+    /// <param name="b"> bullet to be pooled </param>
+    public void SendBullet(GameObject b)
     {
-        
+        b.SetActive(false);
+        b.transform.SetParent(transform);
+        bulletPool.Push(b);
     }
 }
