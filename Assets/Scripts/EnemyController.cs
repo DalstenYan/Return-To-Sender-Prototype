57973348bbfe871f87da18be85fc359f4aa8cb12
0wifi <104783815+0wifi@users.noreply.github.com>
2025-02-17 15:38:31 -0600

    enemy tracking player
    serialized variables in editor:
    - looks at player (bool)
    - look speed
    - look range
    - shoot delay

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,17 +1,54 @@
 using System.Collections;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
 
 public class EnemyController : MonoBehaviour
 {
     private BulletManager bulletManager;
+
+    [Header("Shooting Variables")][Space(5)]
+
+    [Tooltip("Bullet distance per second")][Min (0)]
     [SerializeField] private float bulletSpeed;
+
+    [Tooltip("Instantiation position of bullets")]
     [SerializeField] private Transform shootPosition;
 
+    [Tooltip("Delay between enemy firing bullets")][Min (0)]
+    [SerializeField] private float shootDelay = 0.5f;
+
+    [Space (5)]
+
+    [Header ("Rotation Variables")][Space (5)]
+
+    [Tooltip ("If enemy will track player")]
+    [SerializeField] private bool looksAtPlayer;
+
+    [Tooltip("Rotation speed of enemy (degrees per second) ((i think))")] [Min (0)] 
+    [SerializeField] private float lookSpeed = 30;
+
+    [Tooltip ("Distance that enemy will begin to track the player")] [Min(0)]
+    [SerializeField] private float lookRange = 30;
+
+    private GameObject player;
 
     void Start()
     {
         Activation(transform);
+        player = GameObject.FindWithTag("Player"); print("Enemy detected player:" + player.name);
+    }
+
+    private void Update()
+    {
+        //check look bool & within range
+        if (looksAtPlayer && Vector3.Distance(player.transform.position, transform.position) <= lookRange)
+        {
+            //direction vector
+            Vector3 relativePos = player.transform.position - transform.position;
+            //create new rotation quaternion
+            Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
+            //rotate towards quaternion with lookspeed
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, Time.deltaTime * lookSpeed);
+        }
     }
 
     void OnEnable()
@@ -25,15 +62,20 @@ public class EnemyController : MonoBehaviour
     /// </summary>
     void Shoot()
     {
+        //get bullet from bullet pool
         GameObject bullet = bulletManager.GetBullet();
+
+        //set bullet transform
         bullet.transform.position = shootPosition.position;
         bullet.transform.rotation = transform.rotation;
+
+        //set bullet velocity
         bullet.GetComponent<Rigidbody>().linearVelocity = transform.forward * bulletSpeed;
     }
 
-    IEnumerator ShootLoop()
+    IEnumerator ShootLoop() //basic loop to shoot on a timer with shootDelay
     {
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(shootDelay);
         Shoot();
         StartCoroutine(ShootLoop());
     }
@@ -58,6 +100,6 @@ public class EnemyController : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.DrawRay(shootPosition.position, transform.forward * 1f);
-        //draws ray in shoot direction for visible sightline
+        //draws ray in shoot direction for visible sightline (editor only)
     }
 }
